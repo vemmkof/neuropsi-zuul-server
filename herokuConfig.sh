2f811b4953a803e2be2b874d12ee1b1a193b7d70
Victor Mujica <victor.mujica.aztech@gmail.com>
2020-05-10 12:07:35 -0500

    heroku config

---
+++ herokuConfig.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+heroku config:set USERNAME=zuul-sever
+heroku config:set PASSWORD=b7c59d966e981ab0498809b4f22cdfb1
+heroku config:set PROFILE=dev
+heroku config:set HOSTNAME=protected-caverns-83632.herokuapp.com
+heroku config:set EUREKA_URL=https://eureka-server:b9cd4bdd5f554c576ba9e85b097f2be0@safe-shore-20278.herokuapp.com
