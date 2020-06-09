34beeb20e24764b461389c9c92663009f468b519
Victor Mujica <victor.mujica.aztech@gmail.com>
2020-06-09 13:07:21 -0500

    record service

--- herokuConfig.sh
+++ herokuConfig.sh
@@ -4,4 +4,5 @@ heroku config:set PASSWORD=b7c59d966e981ab0498809b4f22cdfb1
 heroku config:set PROFILE=dev
 heroku config:set HOSTNAME=protected-caverns-83632.herokuapp.com
 heroku config:set OAUTH_URL=http://arcane-caverns-31516.herokuapp.com/
+heroku config:set RECORD_URL=https://secure-temple-30333.herokuapp.com/
 heroku config:set EUREKA_URL=https://eureka-server:b9cd4bdd5f554c576ba9e85b097f2be0@safe-shore-20278.herokuapp.com
